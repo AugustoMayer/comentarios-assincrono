7d19093f72301a489e6b70ff3741ce0c11c5d8a6
Augusto Mayer <augustlmayer@gmail.com>
2026-04-09 16:23:50 -0300

    feat: rota de criação e GET de comentários | persistência de dados no banco

---
+++ api/app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  def index
+    @comments = Comment.order(created_at: :desc)
+    render json: @comments
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:author, :content)
+  end
+end
--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
+
 # Remove um possível server.pid antigo que impede o Rails de subir
 rm -f /app/tmp/pids/server.pid
+
+# Cria o banco se não existir, e roda as migrations pendentes.
+bundle exec rails db:prepare
+
+# Executa o comando principal do container (iniciar o servidor)
 exec "$@"
